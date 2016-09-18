6e0d4fcf2342c8b27300103467142f2f1e408cd9
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2016-09-18 19:01:41 +0300

    Revisited configuration for Linux builds
    Also reverted to the default settings for MacOS builds

--- build.py
+++ build.py
@@ -7,28 +7,15 @@ if __name__ == "__main__":
                             ("4.9", "x86_64", "sjlj", "posix"),
                             ("4.9", "x86", "sjlj", "posix"),
                             ("4.9", "x86", "dwarf2", "posix")]
-    builder = ConanMultiPackager(username='lucteo', mingw_configurations=mingw_configurations, visual_runtimes=["MT", "MD"])
+    builder = ConanMultiPackager(username='lucteo', mingw_configurations=mingw_configurations)
 
-    if platform.system() == "Windows":
-        builder.add_common_builds(visual_versions=[12, 13])
+    builder.add_common_builds()
 
-    if platform.system() == "Linux":
-        for ver in (filter(None,
-                           os.getenv("CONAN_GCC_VERSIONS", "").split(","))
-                    or ["4.8", "4.9", "5.2", "5.3"]):
-            for arch in ["x86", "x86_64"]:
-                builder.add({"arch": arch,
-                             "build_type": "Release",
-                             "compiler": "gcc",
-                             "compiler.version": ver}, {})
+    # Keep only Release builds
+    filtered_builds = []
+    for settings, options in builder.builds:
+        if settings["build_type"] == "Release":
+             filtered_builds.append([settings, options])
+    builder.builds = filtered_builds
 
-    if platform.system() == "Darwin":
-        for ver in (filter(None,
-                           os.getenv("CONAN_APPLE_CLANG_VERSIONS_VERSIONS", "").split(","))
-                    or ["6.0", "6.1", "7.0", "7.3", "8.0"]):
-            for arch in ["x86", "x86_64"]:
-                builder.add({"arch": arch,
-                             "build_type": "Release",
-                             "compiler": "apple-clang",
-                             "compiler.version": ver}, {})
     builder.run()
